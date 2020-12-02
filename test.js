9d5ffffe13530146a1ebf6906d438b1d999bfedb
정상환 <yajang12@gmail.com>
2020-12-02 22:39:00 +0900

    nodejs plus

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-console.log("Hello World")
-while(true){
-    console.log("Hello World")
-}
+// console.log("Hello World")
+// while(true){
+//     console.log("Hello World")
+// }
+
+var fs = require('fs');
+var data = fs.readFileSync('test.txt', 'utf8')
+console.log(data)
